d6413b94c5843f84644fbfd35609637c136c031d
Anjul <anjulsharma010@gmail.com>
2024-10-26 01:25:32 +0530

    api using axios

--- src/components/context/productcontext.js
+++ src/components/context/productcontext.js
@@ -2,14 +2,31 @@
 // 2. create provider
 // 3. consumer
 
-import { createContext, useContext } from "react";
+import axios from "axios";
+import { createContext, useContext, useEffect } from "react";
 
 export const AppContext = createContext();
 
+const API = "https://api.pujakaitem.com/api/products";
+
 export const AppProvider = ({ children }) => {
-  return <AppContext.Provider value={{myName: "Anjul"}}>{children}</AppContext.Provider>;
+  const getProducts = async (url) => {
+    const res = await axios.get(url);
+    const products = await res.data;
+    console.log(products);
+  };
+
+  useEffect(() => {
+    getProducts(API);
+  }, []);
+
+  return (
+    <AppContext.Provider value={{ myName: "Anjul" }}>
+      {children}
+    </AppContext.Provider>
+  );
 };
 
-export const useProductContext = ()=>{
-    return useContext(AppContext)
-}
+export const useProductContext = () => {
+  return useContext(AppContext);
+};
