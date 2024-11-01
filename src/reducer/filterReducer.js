da0562d064e5df57072d14623ebc22ec38c0247e
Anjul <anjulsharma010@gmail.com>
2024-11-01 17:12:52 +0530

    GridView and ListView Done

---
+++ src/reducer/filterReducer.js
@@ -0,0 +1,19 @@
+const filterReducer = (state, action) => {
+  switch (action.type) {
+    case "LOAD_FILTER_PRODUCTS":
+      return {
+        ...state,
+        filter_products: action.payload,
+        allProducts: action.payload,
+      };
+    case "SET_GRIDVIEW":
+      return {
+        ...state,
+        grid_view: true,
+      };
+    default:
+      return state;
+  }
+};
+
+export default filterReducer;
