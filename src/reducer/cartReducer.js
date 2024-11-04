2044c5f035dec27b879c4e540890d3c9ed1b5e0c
Anjul <anjulsharma010@gmail.com>
2024-11-05 02:05:06 +0530

    nothing

--- src/reducer/cartReducer.js
+++ src/reducer/cartReducer.js
@@ -62,7 +62,7 @@ const cartReducer = (state, action) => {
   }
 
   if (action.type === "CART_TOTAL_ITEM") {
-    let updatedItemVal = state.cart.reduce((initialValue, curElem) => {
+    let updatedItemVal = state.cart?.reduce((initialValue, curElem) => {
       let { amount } = curElem;
 
       initialValue = initialValue + amount;
