f0752135aa9e9af85e86f584d6179acac68affbc
Anjul <anjulsharma010@gmail.com>
2024-11-05 00:44:16 +0530

    add to cart

--- src/context/cart_context.js
+++ src/context/cart_context.js
@@ -3,20 +3,20 @@ import reducer from "../reducer/cartReducer";
 
 const cartContext = createContext();
 
-const getLocalCartData = ()=>{
-  let localCartData = localStorage.getItem("sharmaCart")
+const getLocalCartData = () => {
+  let localCartData = localStorage.getItem("sharmaCart");
   if (localCartData == []) {
     return [];
   } else {
     return JSON.parse(localCartData);
   }
-}
+};
 
 const initialState = {
   // cart: [],
   cart: getLocalCartData(),
   total_item: "",
-  total_amount: "",
+  total_price: "",
   shipping_fee: 50000,
 };
 
@@ -32,11 +32,35 @@ export const CartProvider = ({ children }) => {
   };
   //  Add data in local Storage
   useEffect(() => {
+    dispatch({type: "CART_TOTAL_ITEM"})
     localStorage.setItem("sharmaCart", JSON.stringify(state.cart));
   }, [state.cart]);
 
+  // TO clear the cart
+  const clearCart = () => {
+    dispatch({ type: "CLEAR_CART" });
+  };
+
+  // ste increate and decrease functions
+
+  const setDecrease = (id) => {
+    dispatch({ type: "SET_DECREMENT", payload: id });
+  };
+  const setIncrease = (id) => {
+    dispatch({ type: "SET_INCREMENT", payload: id });
+  };
+
   return (
-    <cartContext.Provider value={{ ...state, addToCart, removeItem }}>
+    <cartContext.Provider
+      value={{
+        ...state,
+        addToCart,
+        removeItem,
+        clearCart,
+        setDecrease,
+        setIncrease,
+      }}
+    >
       {children}
     </cartContext.Provider>
   );
